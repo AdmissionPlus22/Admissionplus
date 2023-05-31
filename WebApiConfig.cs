87bda668e355f2584274fc4a8ff63ccdf61d04d4
skpirumd <135026320+skpirumd@users.noreply.github.com>
2023-05-31 13:58:36 +0530

    Add files via upload

---
+++ WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http;
+//using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+
+namespace Admissionplus
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}/{id1}/{id2}/{id3}/{id4}",
+                defaults: new { id = RouteParameter.Optional, id1 = RouteParameter.Optional, id2 = RouteParameter.Optional, id3 = RouteParameter.Optional, id4 = RouteParameter.Optional }
+            );
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+        }
+    }
+}
